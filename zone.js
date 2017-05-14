ed1f6cafbaf46655dd6c8d09039148a780fd5165
MAKIO135 <makio135@gmail.com>
2017-05-14 23:21:52 +0200

    adding cursor events

--- zone.js
+++ zone.js
@@ -12,7 +12,7 @@ function Zone( x, y, w, h ){
     var decayTime = 0.2;
     var susPercent = 0.2;
     var releaseTime = 0.5;
-    
+
     this.env = new p5.Env();
     this.env.setADSR( attackTime, decayTime, susPercent, releaseTime );
     this.env.setRange( attackLevel, releaseLevel );
@@ -31,7 +31,7 @@ function Zone( x, y, w, h ){
 
 Zone.prototype.display = function(){
     stroke( 255 );
-    fill( 255, this.mouseIsOver() ? 255 : 100 );
+    fill( 255, this.over ? 255 : 100 );
     rect( this.x, this.y, this.w, this.h );
 
     noStroke();
@@ -44,6 +44,30 @@ Zone.prototype.mouseIsOver = function(){
                 mouseX <= this.x + this.w &&
                 mouseY >= this.y &&
                 mouseY <= this.y + this.h;
+
+    if( this.over ){
+        this.west = false;
+        this.east = false;
+        this.north = false;
+        this.south = false;
+
+        if( mouseX < this.x + 20 ) this.west = true;
+        else if( mouseX > this.x + this.w - 20 ) this.east = true;
+
+        if( mouseY < this.y + 20 ) this.north = true;
+        else if( mouseY > this.y + this.h - 20 ) this.south = true;
+
+        document.body.style.cursor =
+            this.north && this.west ? 'nw-resize' :
+            this.north && this.east ? 'ne-resize':
+            this.south && this.west ? 'sw-resize':
+            this.south && this.east ? 'se-resize' :
+            this.west ? 'w-resize' :
+            this.east ? 'e-resize' :
+            this.north ? 'n-resize' :
+            this.south ? 's-resize':
+            'copy'
+    }
     return this.over;
 };
 
